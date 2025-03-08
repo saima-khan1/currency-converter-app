7cf2219128335c3a1dd54a476303aea96b927da0
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-03-08 09:27:03 +0100

    added functionality

--- currency-converter-app/src/components/CurrencyConverter.tsx
+++ currency-converter-app/src/components/CurrencyConverter.tsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+
 const CurrencyConverter = () => {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setFromCurrency] = useState("pkr");
+  const [toCurrency, setToCurrency] = useState("inr");
+  const [convertedCurrency, setConvertedCurrency] = useState(null);
+
   return (
     <div>
-      <h1>Currency Converter App </h1>
+      <h1>Currency Converter</h1>
+      <label>Amount</label>
+      <input
+        value={amount}
+        type="number"
+        onChange={(e) => setAmount(e.target.value)}
+      ></input>
+      <label>From:</label>
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
+        <option>inr</option>
+        <option>pkr</option>
+        <option>usd</option>
+        <option>krona</option>
+      </select>
+
+      <label>To:</label>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
+        <option>inr</option>
+        <option>pkr</option>
+        <option>usd</option>
+        <option>krona</option>
+      </select>
+      <button>calculate</button>
+      <h1>
+        {amount}
+        {"" + fromCurrency}={convertedCurrency}
+        {"" + toCurrency}
+      </h1>
     </div>
   );
 };
