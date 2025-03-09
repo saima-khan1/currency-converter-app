cbe3c73d808fa6df8f6c5616791776b24c454474
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-03-09 23:11:12 +0100

    fetching data

--- currency-converter-app/src/components/CurrencyConverter.tsx
+++ currency-converter-app/src/components/CurrencyConverter.tsx
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CurrencyConverter = () => {
+  const [currencies, setCurrencies] = useState([""]);
   const [amount, setAmount] = useState("");
-  const [fromCurrency, setFromCurrency] = useState("pkr");
-  const [toCurrency, setToCurrency] = useState("inr");
+  const [fromCurrency, setFromCurrency] = useState("");
+  const [toCurrency, setToCurrency] = useState("");
   const [convertedCurrency, setConvertedCurrency] = useState(null);
+  async function handleCurrencyConversion() {
+    const response = await fetch(`https://api.frankfurter.dev/v1/currencies`);
+    const data = await response.json();
 
+    setCurrencies(Object.keys(data));
+    const responseData = await fetch(
+      `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+    );
+
+    const queryData = await responseData.json();
+    setConvertedCurrency(queryData.rates[toCurrency]);
+  }
+  useEffect(() => {
+    handleCurrencyConversion();
+  }, []);
   return (
     <div>
       <h1>Currency Converter</h1>
@@ -20,10 +35,11 @@ const CurrencyConverter = () => {
         value={fromCurrency}
         onChange={(e) => setFromCurrency(e.target.value)}
       >
-        <option>inr</option>
-        <option>pkr</option>
-        <option>usd</option>
-        <option>krona</option>
+        {currencies.map((curVal) => (
+          <option key={curVal} value={curVal}>
+            {curVal}
+          </option>
+        ))}
       </select>
 
       <label>To:</label>
@@ -31,12 +47,13 @@ const CurrencyConverter = () => {
         value={toCurrency}
         onChange={(e) => setToCurrency(e.target.value)}
       >
-        <option>inr</option>
-        <option>pkr</option>
-        <option>usd</option>
-        <option>krona</option>
+        {currencies.map((curVal) => (
+          <option key={curVal} value={curVal}>
+            {curVal}
+          </option>
+        ))}
       </select>
-      <button>calculate</button>
+      <button onClick={handleCurrencyConversion}>calculate</button>
       <h1>
         {amount}
         {"" + fromCurrency}={convertedCurrency}
